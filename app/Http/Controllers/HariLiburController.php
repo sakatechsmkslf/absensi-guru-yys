2022c5fdc14ba3d7429e275fa9905904f9d392d2
hexaviate <hexaviate@gmail.com>
2025-09-06 11:10:12 +0700

    membuat crud hari libur (belum ditest)

--- app/Http/Controllers/HariLiburController.php
+++ app/Http/Controllers/HariLiburController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\HariLibur;
 use App\Models\Instansi;
 use App\Models\Tapel;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -35,12 +36,21 @@ class HariLiburController extends Controller
      */
     public function store(Request $request)
     {
+        $user = auth()->user();
+
+
         $validate = Validator::make($request->all(), [
             'tapel_id' => 'required|exists:tapels,id',
             'instansi_id' => 'required|exists:instansis,id',
             'keterangan' => 'required|string',
             'tanggal' => 'required',
         ]);
+
+        if ($validate->fails()) {
+            return redirect()->route('instansi.create')->withErrors($validate)->withInput();
+        }
+
+        $instansiOperator = $user->instansi()->where('instansi_id', $request->instansi_id)->first();
     }
 
     /**
